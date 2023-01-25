4584bb06b766fa1a2c06ca3dbbe0b8517824b58f
daniel-georgiev-dgmfb <daniel.georgiev73@gmail.com>
2023-01-25 18:43:00 +0200

    splitting the projects

---
+++ RainbowTablesAsync/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static async Task Main(string[] args)
+    {
+        //var stopwatch = new Stopwatch();
+        await AsyncRainbowTableGenerator.Run(args[0].ToCharArray());
+        Console.ReadLine();
+    }
+
+
+}
