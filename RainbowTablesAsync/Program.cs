d3d557a576e7bcc5545e43d018f96954ec933b9b
daniel-georgiev-dgmfb <daniel.georgiev73@gmail.com>
2023-02-02 20:44:43 +0200

    foo

--- RainbowTablesAsync/Program.cs
+++ RainbowTablesAsync/Program.cs
@@ -1,9 +1,13 @@
-﻿class Program
+﻿using System.Text;
+
+class Program
 {
     static async Task Main(string[] args)
     {
         //var stopwatch = new Stopwatch();
-        await AsyncRainbowTableGenerator.Run(args[0].ToCharArray());
+        //await AsyncRainbowTableGenerator.Run(args[0].ToCharArray());
+        var sb = new StringBuilder();
+        await AsyncRainbowTableGeneratorRec.Run(args[0].ToCharArray(), 0, new char[9], sb);
         Console.ReadLine();
     }
 }
